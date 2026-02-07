323d78df299b319c5a4155b008e2bc8ba9963137
joejianlang <102838265+joejianlang@users.noreply.github.com>
2026-02-07 11:55:38 -0500

    Update fetch_service.ts
    更新了

--- lib/services/fetch_service.ts
+++ lib/services/fetch_service.ts
@@ -11,7 +11,7 @@ if (!serviceRoleKey) {
 }
 
 const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!,
     serviceRoleKey || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
